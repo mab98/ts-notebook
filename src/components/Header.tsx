75e24a8a6ef3b917a8a16ef474ffd638e3094351
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-05-24 20:38:29 +0500

    added dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../App.css';
-import styled from "styled-components";
+import styled, { ThemeContext } from "styled-components";
+import Switch from "react-switch";
+import { shade } from "polished";
 
 const HeaderSC = styled.header`
-  height: 4rem;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
   width: 100vw;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  background: ${({ theme }) => theme.colors.primary};
+
 `
 const H1 = styled.h1`
   margin-bottom: 0;
 `
 const HeaderLogo = styled.span`
-margin-top: 4px;
+  margin-top: 4px;
   position: absolute;
   top: 4px;
   left: 15px;
@@ -20,19 +28,28 @@ const HeaderLogoText = styled.span`
   font-size: 40px;
   font-family: 'Girassol', cursive;
   color: chocolate;
+  color: ${({ theme }) => theme.colors.text};
   position: absolute;
   top: -5px;
 `
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  text: string;
+  toggleTheme(): void;
+}
+
+const Header: React.FC<HeaderProps> = ({ text, toggleTheme }) => {
+  const { title, colors } = useContext(ThemeContext)
   return (
     <HeaderSC>
       <H1>
         <HeaderLogo>
           <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="Google Keep Logo" />
-          <HeaderLogoText>Notebook</HeaderLogoText>
+          <HeaderLogoText>{text}</HeaderLogoText>
         </HeaderLogo>
       </H1>
+      <Switch onChange={toggleTheme} checked={title === 'dark'} checkedIcon={false} uncheckedIcon={false} height={15} width={35} handleDiameter={20} offColor={shade(.15, '#e4a477')} offHandleColor="#D63AF9"
+        onColor={colors.secondary} />
     </HeaderSC>
   )
 }
---
+++ src/modeStyles/themes.ts
@@ -0,0 +1,19 @@
+export const light = {
+  title: 'light',
+  colors: {
+    primary: '#fff',
+    secondary: '#C62E65',
+    background: '#f5f5f5',
+    text: 'chocolate',
+  },
+};
+
+export const dark = {
+  title: 'dark',
+  colors: {
+    primary: '#C62E65',
+    secondary: '#D63AF9',
+    background: '#333',
+    text: '#fff',
+  },
+};
