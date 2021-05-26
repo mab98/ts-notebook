1d835c21aab24cf8ca3f08dc984836564412186c
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-05-26 15:48:40 +0500

    added persistence

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,4 +2,31 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducers } from './reducers';
 
-export const store = createStore(reducers, {}, composeWithDevTools());
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
+export const store = createStore(
+  reducers,
+  persistedState,
+  composeWithDevTools()
+);
+type RootState = ReturnType<typeof store.getState>;
+function saveToLocalStorage(state: RootState) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
