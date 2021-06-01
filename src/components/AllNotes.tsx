f268b8f7129b21668803cc1bcb9a3517026eea98
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-06-01 14:10:40 +0500

    added drag and drop

--- src/components/AllNotes.tsx
+++ src/components/AllNotes.tsx
@@ -6,17 +6,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 import { useSelector } from 'react-redux';
 import { StoreState } from '../store/reducers';
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 
 const AllNotesSC = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: baseline;
-  width: 80vw;
+  width: 95vw;
 `
 
-const NoNotes = styled.p`
+const NoNotes = styled.span`
   margin: 2rem auto;
   color: lightgray;
   font-weight: bold;
@@ -27,13 +28,37 @@ const NoNotes = styled.p`
 
 const AllNotes: React.FC = () => {
   const { storeNotes } = useSelector((state: StoreState) => state)
-  return (
-    <AllNotesSC>
-      { storeNotes.notes[0]
-        ? storeNotes.notes.map((note: INote, key: number) => <Note key={note.id} note={note} />)
-        : <NoNotes> <FontAwesomeIcon size='5x' icon={faLightbulb} /> <br />Notes you add appear here </NoNotes>}
-    </AllNotesSC>
-  )
+  const [draggableNotes, setDraggableNotes] = React.useState(storeNotes.notes);
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    const items = Array.from(draggableNotes);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setDraggableNotes(items);
+  }
+  React.useEffect(() => {
+    setDraggableNotes(storeNotes.notes)
+  }, [storeNotes]);
+  return (<>
+    {storeNotes.notes[0] ?
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        < Droppable droppableId='notes' direction="horizontal" type="column">
+          {provided => (
+            <AllNotesSC {...provided.droppableProps} ref={provided.innerRef}>
+              { draggableNotes.map((note: INote, index: number) =>
+                <Draggable key={note.id} draggableId={note.id} index={index}>
+                  {provided => (
+                    <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+                      <Note note={note} /></div>
+                  )}
+                </Draggable>,
+                (provided.placeholder))}
+            </AllNotesSC>
+          )}
+        </Droppable>
+      </DragDropContext >
+      : <NoNotes> <FontAwesomeIcon size='5x' icon={faLightbulb} /> <br />Notes you add appear here </NoNotes>}
+  </>)
 }
 
 export default AllNotes
