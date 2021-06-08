a47a96bafb43dedb38e89a2e632fdf98e2ea39b7
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-06-08 15:03:17 +0500

    tried react-grid-layout

--- src/components/AllNotes.tsx
+++ src/components/AllNotes.tsx
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
 import { useSelector } from 'react-redux';
 import { StoreState } from '../store/reducers';
+import ReactGridLayout, { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveGridLayout = WidthProvider(ReactGridLayout);
 
 const AllNotesSC = styled.div`
   display: flex;
@@ -15,7 +17,6 @@ const AllNotesSC = styled.div`
   align-items: baseline;
   width: 80vw;
 `
-
 const NoNotes = styled.p`
   margin: 2rem auto;
   color: lightgray;
@@ -24,16 +25,34 @@ const NoNotes = styled.p`
   text-align: center;
   line-height: 4rem;
 `
+interface ILayout {
+  i: string
+  x: number
+  y: number
+  w: number
+  h: number
+}
+
+const generateLayout = (notes: INote[]): ILayout[] =>
+  notes.reduce((acc: ILayout[], item: INote, index) => acc.concat({ i: item.id, x: index + 1, y: 4, w: 1, h: 1 }), []);
 
 const AllNotes: React.FC = () => {
   const { storeNotes } = useSelector((state: StoreState) => state)
   return (
     <AllNotesSC>
       { storeNotes.notes[0]
-        ? storeNotes.notes.map((note: INote, key: number) => <Note key={note.id} note={note} />)
+        ?
+        <ResponsiveGridLayout
+          className="layout"
+          layout={generateLayout(storeNotes.notes)}
+          cols={12}
+          rowHeight={281}
+          isDraggable
+        >
+          {storeNotes.notes.map((note: INote) => <Note key={note.id} note={note} />)}
+        </ResponsiveGridLayout>
         : <NoNotes> <FontAwesomeIcon size='5x' icon={faLightbulb} /> <br />Notes you add appear here </NoNotes>}
     </AllNotesSC>
   )
 }
-
 export default AllNotes
