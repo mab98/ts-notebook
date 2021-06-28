3bf02aecef0a40c3e1d07b959102f953870777e2
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-06-28 13:16:59 +0500

    added wrapper component for header test, passing header test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,15 +1,21 @@
 import { render, screen } from '@testing-library/react';
-import React, { useState } from 'react';
-import { DefaultTheme } from 'styled-components';
+import React from 'react';
 import { dark, light } from '../modeStyles/themes';
 import usePersistedState from '../utils/usePersistedState';
 import Header from './Header';
+import { ThemeProvider, DefaultTheme } from "styled-components";
 
-const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
-const toggleTheme = () => { setTheme(theme.title === 'light' ? dark : light); };
+const TestHeaderWrapperComponent = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+  const toggleTheme = () => { setTheme(theme.title === 'light' ? dark : light); };
+  return (<ThemeProvider theme={theme}>
+    <Header text="Notebook" href='#' toggleTheme={toggleTheme} />
+  </ThemeProvider>
+  )
+}
 
 test("renders Notebook text in the Header", () => {
-  render(<Header text="Notebook" href='#' toggleTheme={toggleTheme} />);
-  const HeaderElement = screen.getByAltText('Notebook')
+  render(<TestHeaderWrapperComponent />);
+  const HeaderElement = screen.getByText('Notebook')
   expect(HeaderElement).toBeInTheDocument()
 })
